001a03647196de0c49584b50875a1b997a628a4c
ViniciusCavalcante2000 <viniciuscavalcanterj@gmail.com>
2022-01-27 07:47:18 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,17 @@
+function compare(a, b) {
+    if (a === b) {
+      console.log('Os números ', a, 'e ', b, 'são iguais.')
+    } else {
+       console.log('Os números ', a, 'e ', b, 'não iguais.')
+    }
+    let soma = (+a + +b);
+    if (soma < 10) {
+      console.log('Sua soma é',soma, "que é menor do que 10 e menor do que 20.")
+    } else if (soma > 10 & soma < 20) {
+       console.log('Sua soma é', soma, ', que é maior do que 10 e menor do que 20.')
+    } else {
+       console.log('Sua soma é', soma, ', que é maior do que 10 e maior do que 20.')
+    }
+  }
+  
+  compare(prompt('Digite o primeiro número'), prompt('Digite o segundo número'))
