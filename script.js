c5dfe5591d546cc8f2f616d3e40e2c8f8834c408
ViniciusCavalcante2000 <viniciuscavalcanterj@gmail.com>
2022-01-27 08:05:14 -0300

    MOdificado para reagir quando a soma for igual a 10 ou igual a 20

--- script.js
+++ script.js
@@ -6,9 +6,13 @@ function compare(a, b) {
     }
     let soma = (+a + +b);
     if (soma < 10) {
-      console.log('Sua soma é',soma, "que é menor do que 10 e menor do que 20.")
-    } else if (soma > 10 & soma < 20) {
+      console.log('Sua soma é',soma, 'que é menor do que 10 e menor do que 20.')
+    } else if (soma === 10 & soma < 20) {
+      console.log('Sua soma é', soma, ', que é igual a 10 e menor do que 20.')
+   }  else if (soma > 10 & soma < 20) {
        console.log('Sua soma é', soma, ', que é maior do que 10 e menor do que 20.')
+   }  else if (soma > 10 & soma === 20) {
+         console.log('Sua soma é', soma, ', que é maior do que 10 e é igual a 20.')
     } else {
        console.log('Sua soma é', soma, ', que é maior do que 10 e maior do que 20.')
     }
