dbf3e17e8b60c11cf28917e1de0a22e811ee66f0
ViniciusCavalcante2000 <viniciuscavalcanterj@gmail.com>
2022-01-30 07:35:38 -0300

    String tem apenas uma linha. As virgulas foram substituidadas por plus. Tambem foram incluidas respostas quando a soma é igual a dez ou a vinte

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function compare(a, b) {
     if (a === b) {
-      console.log('Os números ', a, 'e ', b, 'são iguais.')
+      iguais = 'Os números ' + a + ' e ' + b + ' são iguais. '
     } else {
-       console.log('Os números ', a, 'e ', b, 'não iguais.')
+       iguais = 'Os números ' + a + ' e ' + b + ' não são iguais. '
     }
     let soma = (+a + +b);
     if (soma < 10) {
-      console.log('Sua soma é',soma, 'que é menor do que 10 e menor do que 20.')
+      console.log(iguais + 'Sua soma é ' + soma + ',' + ' que é menor do que 10 e menor do que 20.')
     } else if (soma === 10 & soma < 20) {
-      console.log('Sua soma é', soma, ', que é igual a 10 e menor do que 20.')
+      console.log(iguais + 'Sua soma é', soma, ', que é igual a 10 e menor do que 20.')
    }  else if (soma > 10 & soma < 20) {
-       console.log('Sua soma é', soma, ', que é maior do que 10 e menor do que 20.')
+       console.log(iguais + 'Sua soma é', soma, ', que é maior do que 10 e menor do que 20.')
    }  else if (soma > 10 & soma === 20) {
-         console.log('Sua soma é', soma, ', que é maior do que 10 e é igual a 20.')
+         console.log(iguais + 'Sua soma é', soma, ', que é maior do que 10 e é igual a 20.')
     } else {
-       console.log('Sua soma é', soma, ', que é maior do que 10 e maior do que 20.')
+       console.log(iguais + 'Sua soma é', soma, ', que é maior do que 10 e maior do que 20.')
     }
   }
   
